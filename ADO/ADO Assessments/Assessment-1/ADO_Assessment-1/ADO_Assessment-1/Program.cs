50959496e9682d655b377351581171a778abce30
Sai Naga Hanuman Gokavarapu <sainagag@infinite.com>
2024-12-11 11:59:56 +0530

    The Assessment IN ADO File

---
+++ ADO/ADO Assessments/Assessment-1/ADO_Assessment-1/ADO_Assessment-1/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ADO_Assessment_1   
+{
+      public class Program
+{
+    public static SqlConnection connection = null;
+    public static SqlCommand cmd = null;
+    public static IDataReader reader = null;
+    void Operations()
+    {
+        
+        connection = new SqlConnection("Data Source=ICS-LT-D244D6BX;Initial Catalog=ADO_Assessment;" +
+                    "Integrated Security=true;");
+       
+        connection.Open();
+       
+        cmd = new SqlCommand("sp_productdetails", connection);
+        cmd.CommandType = CommandType.StoredProcedure;
+
+        Console.WriteLine("Enter the product id:");
+        int ProductId = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the Product_name:");
+        String ProductName = Console.ReadLine();
+        Console.WriteLine("Enter the Product price:");
+        float Price = Convert.ToSingle(Console.ReadLine());
+        Console.WriteLine("Enter the discounted Price:");
+        float DiscountedPrice = Convert.ToSingle(Console.ReadLine());
+        DiscountedPrice = Price - 0.10f;
+
+
+        
+        cmd.Parameters.Add(new SqlParameter("@productId", SqlDbType.Int)).Value = ProductId;
+        cmd.Parameters.Add(new SqlParameter("@productName", SqlDbType.VarChar, 40)).Value = ProductName;
+        cmd.Parameters.Add(new SqlParameter("@Price", SqlDbType.Float)).Value = Price;
+        cmd.Parameters.Add(new SqlParameter("@DiscountedPrice", SqlDbType.Float)).Value = DiscountedPrice;
+
+
+        int result = cmd.ExecuteNonQuery();
+        if (result > 0)
+        {
+            Console.WriteLine("Success:");
+        }
+        else
+        {
+            Console.WriteLine("Fail:");
+        }
+        connection.Close();
+
+    }
+    void select()
+    {
+        connection = new SqlConnection("Data source = ICS-LT-D244D6GL; database = ADOAssesment; trusted_connection = true;");
+        Console.WriteLine("Successfully connected:");
+       
+        connection.Open();
+
+        cmd = new SqlCommand("sp_productdetails", connection);
+        cmd.CommandType = CommandType.StoredProcedure;
+        Console.WriteLine("Enter the product id:");
+        int ProductId = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter the Product_name:");
+        String ProductName = Console.ReadLine();
+        Console.WriteLine("Enter the Product price:");
+        float Price = Convert.ToSingle(Console.ReadLine());
+        Console.WriteLine("Enter the discounted Price:");
+        float DiscountedPrice = Convert.ToSingle(Console.ReadLine());
+
+        cmd.Parameters.Add(new SqlParameter("@productId", SqlDbType.Int)).Value = ProductId;
+        cmd.Parameters.Add(new SqlParameter("@productName", SqlDbType.VarChar, 40)).Value = ProductName;
+        cmd.Parameters.Add(new SqlParameter("@Price", SqlDbType.Float)).Value = Price;
+        cmd.Parameters.Add(new SqlParameter("@DiscountedPrice", SqlDbType.Float)).Value = DiscountedPrice;
+
+
+
+        reader = cmd.ExecuteReader();
+        while (reader.Read())
+        {
+            Console.WriteLine("ProductId is:" + reader[0] + "ProductName is:" + reader[1] + "Price:" + reader[2] + "DiscountedPrice:" + reader[3]);
+        }
+    }
+    public static void Main(string[] args)
+    {
+        Program p = new Program();
+        p.Operations();
+        p.select();
+        Console.Read();
+    }
+}
+}
---
+++ ADO/ADO Assessments/Assessment-1/ADO_Assessment-1/ADO_SQL.sql
@@ -0,0 +1,76 @@
+create database ADO_Assessment
+use ADO_Assessment
+
+create table CalculateCourse_duration
+(
+C_id Varchar(15)Not null,
+C_name Varchar(25),
+Start_date Date ,
+End_date Date,
+Fee int Not null
+)
+ 
+Insert into CalculateCourse_duration 
+values
+('DN003','DotNet','2018-02-01','2018-02-28',15000),
+('DV004','DataVisualization','2018-03-01','2018-04-15',15000),
+('JA002','AdvancedJava','2018-01-02','2018-01-20',10000),
+('JC001','CoreJava','2018-01-02','2018-01-12',3000)
+
+--1.Create a Function to calculate the course duration for the above relation by receiving start date and end date as input.
+
+create or alter function fn_CalculateCourse_duration(@start_date date,@end_date date)
+  returns int
+  as
+  begin
+  declare @duration int
+  set @duration = datediff(day, @start_date, @end_date)
+  return @duration
+  end
+ 
+  select dbo.fn_CalculateCourse_duration(Start_date,End_date) Course_durationdays
+  from CalculateCourse_duration;
+
+
+--2nd (1). Create a trigger to display the Course Name and Start date of the course.
+
+CREATE TABLE T_CourseInfo(
+C_Name VARCHAR(30),
+Start_date DATE)
+ 
+
+create or alter trigger tgrDisplay
+on CalculateCourse_duration
+for insert
+as
+begin
+insert into CalculateCourse_duration(CName,StartDate)
+select CName,StartDate from inserted
+end
+ 
+INSERT INTO CalculateCourse_duration VALUES
+('CA001','css','2020-10-12','2020-11-22',9000)
+ 
+SELECT * FROM T_CourseInfo
+
+
+
+
+--3rd
+
+create table ProductsDetails(ProductId int, ProductName varchar(30), Price Float, DiscountedPrice Float);
+create procedure sp_productdetails
+@ProductId int,
+@ProductName varchar(30),
+@Price Float,
+@DiscountedPrice Float
+as
+Begin
+insert into ProductsDetails(ProductId, ProductName, Price, DiscountedPrice) values(@ProductId, @ProductName, @Price, @DiscountedPrice);
+end
+
+go
+drop procedure sp_productdetails;
+drop table ProductsDetails;
+
+select * from ProductsDetails;
