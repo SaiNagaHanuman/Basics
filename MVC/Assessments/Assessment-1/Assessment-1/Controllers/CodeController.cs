6ebac8a921f9813f1c783c189defcd1b28d57da9
Sai Naga Hanuman Gokavarapu <sainagag@infinite.com>
2025-01-02 19:56:05 +0530

    Assessment-1

---
+++ MVC/Assessments/Assessment-1/Assessment-1/Controllers/CodeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Assessment_1.Models;
+
+namespace Assessment_1.Controllers
+{
+    public class CodeController : Controller
+    {
+        private northwindEntities db = new northwindEntities();
+
+        // Action 1: Return all customers residing in Germany
+        public ActionResult CustomersInGermany()
+        {
+            var germanCustomers = db.Customers.Where(c => c.Country == "Germany").ToList();
+            return View(germanCustomers); // Pass data to View
+        }
+
+        // Action 2: Return customer details with orderId == 10248
+        public ActionResult CustomerDetailsWithOrder()
+        {
+            var customerDetails = db.Orders
+                .Where(order => order.OrderID == 10248)
+                .Select(order => new
+                {
+                    order.Customer.CustomerID,
+                    order.Customer.CompanyName,
+                    order.Customer.ContactName,
+                    order.Customer.Country,
+                    order.OrderDate
+                }).FirstOrDefault();
+
+            return View(customerDetails); // Pass data to View
+        }
+    }
+}
