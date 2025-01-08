a9cfad138f2448c76529fef09ae24aa8d80c8ecd
Sai Naga Hanuman Gokavarapu <sainagag@infinite.com>
2025-01-08 11:13:59 +0530

    Web Api

---
+++ Web Api/Assesssments/Assessment-1/Assessment-1/Controllers/CountryController1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Assessment_1.Models;
+
+
+namespace Assessment_1.Controllers
+{
+    public class CountryController : ApiController
+    {
+        private static List<Country> countries = new List<Country>
+        {
+            new Country { ID = 1, CountryName = "India", Capital = "New Delhi" },
+            new Country { ID = 2, CountryName =  "Singapore", Capital = "Singapore"}
+        };
+
+        // GET: api/Country
+        [HttpGet]
+        public IHttpActionResult GetAllCountries()
+        {
+            return Ok(countries);
+        }
+
+        // GET: api/Country/1
+        [HttpGet]
+        public IHttpActionResult GetCountry(int id)
+        {
+            var country = countries.FirstOrDefault(c => c.ID == id);
+            if (country == null)
+                return NotFound();
+
+            return Ok(country);
+        }
+
+        // POST: api/Country
+        [HttpPost]
+        public IHttpActionResult AddCountry(Country country)
+        {
+            if (country == null)
+                return BadRequest("Invalid data.");
+
+            countries.Add(country);
+            return Created($"api/Country/{country.ID}", country);
+        }
+
+        // PUT: api/Country/1
+        [HttpPut]
+        public IHttpActionResult UpdateCountry(int id, Country updatedCountry)
+        {
+            var country = countries.FirstOrDefault(c => c.ID == id);
+            if (country == null)
+                return NotFound();
+
+            country.CountryName = updatedCountry.CountryName;
+            country.Capital = updatedCountry.Capital;
+            return Ok(country);
+        }
+
+        // DELETE: api/Country/1
+        [HttpDelete]
+        public IHttpActionResult DeleteCountry(int id)
+        {
+            var country = countries.FirstOrDefault(c => c.ID == id);
+            if (country == null)
+                return NotFound();
+
+            countries.Remove(country);
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+    }
+}
